36d314abce622167ce367f08175993f4f07672a5
Mihnea Dobrescu-Balaur <mihnea@linux.com>
2013-10-18 19:55:43 +0300

    add a dummy todo

--- src-cljs/todo.cljs
+++ src-cljs/todo.cljs
@@ -1,1 +1,8 @@
-(ns cljs-todo.todo)
+(ns cljs-todo.todo
+  (:use-macros
+   [dommy.macros :only [node sel sel1]])
+  (:require
+   [dommy.core :as dommy]))
+
+
+(dommy/append! (sel1 :#todos-div) (node [:p "Make cljs todo app"]))
